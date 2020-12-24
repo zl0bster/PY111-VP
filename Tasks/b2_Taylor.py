a177ce9410292cbd202aeef3ed90e12e90a1342b
zl0bster <s00per@ya.ru>
2020-12-24 18:37:26 +0300

    sin count by Taylor raw realized and tested

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,6 +2,10 @@
 Taylor series
 """
 from typing import Union
+from math import factorial
+from itertools import count
+
+ACCURACY = 0.0001
 
 
 def ex(x: Union[int, float]) -> float:
@@ -22,5 +26,19 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+
+    def item(n):
+        return pow(-1, n - 1) * pow(x, ((n * 2) - 1)) / factorial((2 * n) - 1)
+
+    sum = 0
+    for i in count(1):
+        value = item(i)
+        print(value)
+        sum += value
+        print(sum)
+        if abs(value) <= ACCURACY:
+            return sum
+
+
+if __name__ == '__main__':
+    print(sinx(3.14159 / 2))
