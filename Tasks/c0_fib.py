8407d2ba9c343f927f91b19616821842a54c80ee
zl0bster <s00per@ya.ru>
2020-12-24 18:54:23 +0300

    fib count by iterative and recursive way raw realized and tested

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,8 +5,13 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib_recursive(n - 2) + fib_recursive(n - 1)
 
 
 def fib_iterative(n: int) -> int:
@@ -16,5 +21,19 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    f1 = 0
+    f2 = 1
+    fn = 0
+    while n > 1:
+        fn = f1 + f2
+        f1 = f2
+        f2 = fn
+        n -= 1
+
+    return fn
