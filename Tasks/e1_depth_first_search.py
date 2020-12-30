f9158b3057c59788c17b72c40feef4e6fe2f2b01
zl0bster <s00per@ya.ru>
2020-12-30 19:40:56 +0300

    depth search is realized and tested seems it works

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,5 +1,12 @@
 from typing import Hashable, List
 import networkx as nx
+from collections import deque
+import matplotlib.pyplot as plt
+
+
+def show_graph(g: nx.Graph):
+    nx.draw(g, with_labels=True)
+    plt.show()
 
 
 def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +17,31 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    visitedNodes = {node: False for node in g.nodes}
+    path = []
+    visitedNodes[start_node] = True
+    waitingNodes = deque()
+    waitingNodes.append(start_node)
+    while waitingNodes:
+        currentNode = waitingNodes.pop()
+        path.append(currentNode)
+        visitedNodes[currentNode] = True
+        for node in g.neighbors(currentNode):
+            if not visitedNodes[node]:
+                visitedNodes[node] = True
+                waitingNodes.append(node)
+    return path
+
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from("ABCDEFG")
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'C'),
+        ('B', 'D'),
+        ('B', 'E'),
+        ('C', 'F'),
+        ('E', 'G'),
+    ])
+    print(dfs(graph, 'A'))
