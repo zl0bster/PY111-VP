c2cbedca96931554c85d593df815cb76220973fc
zl0bster <s00per@ya.ru>
2020-12-30 21:13:16 +0300

    stairway direct way tests done

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -9,13 +9,29 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :return: minimal cost of getting to the top
     """
 
-    def get_cost(n):
-        if n == 0 or n == 1:
-            return stairway[n]
-        return stairway[n] + min(get_cost(n - 1), get_cost(n - 2))
+    def lazy_way(stairway):
+        def get_cost(n):
+            if n == 0 or n == 1:
+                return stairway[n]
+            return stairway[n] + min(get_cost(n - 1), get_cost(n - 2))
 
-    return get_cost(len(stairway) - 1)
+        return get_cost(len(stairway) - 1)
 
+    def direct_way(stairway):
+        last = len(stairway)
+        nodeCost = [0] * last
+        nodeCost[0] = stairway[0]
+        nodeCost[1] = stairway[1]  # +stairway[1]
+        for i in range(2, last):
+            nodeCost[i] = stairway[i] + min(nodeCost[i - 1], nodeCost[i - 2])
+            print(i, stairway[i], nodeCost[i], min(stairway[i - 1], stairway[i - 2]))
+        print('*' * 20)
+        return nodeCost[-1]
+
+    return direct_way(stairway)
+
+    # pos = 0
+    # last = len(stairway)
     # minCost = stairway[0]
     # pos = 0
     # last = len(stairway)
