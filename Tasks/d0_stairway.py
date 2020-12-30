12086bf9f6b8084ffcc67e986142f6575d81f28e
zl0bster <s00per@ya.ru>
2020-12-30 20:05:45 +0300

    stairway try

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -8,42 +8,46 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: list of ints, where each int is a cost of appropriate step
     :return: minimal cost of getting to the top
     """
-    # minCost = stairway[0]
-    # pos = 0
-    # last = len(stairway)
-    # while pos < last - 1:
-    #     nextOne = stairway[pos + 1]
-    #     afterNext = stairway[pos + 2]
-    #     if (nextOne >= afterNext) or (pos == last - 2):
-    #         pos += 2
-    #         minCost += afterNext
-    #     else:
-    #         pos += 1
-    #         minCost += nextOne
-    #     print(pos, stairway[pos], minCost)
-    # if pos < (last - 1):
-    #     minCost += stairway[last - 1]
-    # print(minCost)
-    # print('*' * 20)
-    # return minCost
-
-    pos = len(stairway) - 1
-    minCost = stairway[pos]
-    while pos > 1:
-        nextOne = stairway[pos - 1]
-        afterNext = stairway[pos - 2]
-        if (nextOne >= afterNext) or (pos == 2):
-            pos -= 2
+    minCost = stairway[0]
+    pos = 0
+    last = len(stairway)
+    while pos < last - 1:
+        nextOne = stairway[pos + 1]
+        if pos == last - 2:
+            pos += 1
+            minCost += nextOne
+            continue
+        afterNext = stairway[pos + 2]
+        if nextOne >= afterNext:
+            pos += 2
             minCost += afterNext
         else:
-            pos -= 1
+            pos += 1
             minCost += nextOne
         print(pos, stairway[pos], minCost)
-    if pos == 1:
-        minCost += stairway[0]
+    if pos < (last - 1):
+        minCost += stairway[last - 1]
     print(minCost)
     print('*' * 20)
     return minCost
+
+    # pos = len(stairway) - 1
+    # minCost = stairway[pos]
+    # while pos > 1:
+    #     nextOne = stairway[pos - 1]
+    #     afterNext = stairway[pos - 2]
+    #     if (nextOne >= afterNext) or (pos == 2):
+    #         pos -= 2
+    #         minCost += afterNext
+    #     else:
+    #         pos -= 1
+    #         minCost += nextOne
+    #     print(pos, stairway[pos], minCost)
+    # if pos == 1:
+    #     minCost += stairway[0]
+    # print(minCost)
+    # print('*' * 20)
+    # return minCost
 
 
 if __name__ == '__main__':
