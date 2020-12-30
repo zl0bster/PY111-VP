12631adee1601260e65e234facb37e2b3e8a6f8e
zl0bster <s00per@ya.ru>
2020-12-30 18:51:13 +0300

    width search is realized and tested

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,5 +1,12 @@
 from typing import Hashable, List
 import networkx as nx
+import matplotlib.pyplot as plt
+from collections import deque
+
+
+def show_graph(g: nx.Graph):
+    nx.draw(g, with_labels=True)
+    plt.show()
 
 
 def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +17,42 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    visited = {node: False for node in g.nodes}
+    path = []
+    waitNodes = deque()
+    waitNodes.append(start_node)
+    visited[start_node] = True
+    while waitNodes:
+        currentNode = waitNodes.popleft()
+        path.append(currentNode)
+        for neighbour in g.neighbors(currentNode):
+            if not visited[neighbour]:
+                waitNodes.append(neighbour)
+                visited[neighbour] = True
+
+    return path
+
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from("ABCDEFGHIJ")
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'F'),
+        ('B', 'G'),
+        ('F', 'G'),
+        ('G', 'C'),
+        ('G', 'H'),
+        ('G', 'I'),
+        ('C', 'H'),
+        ('I', 'H'),
+        ('H', 'D'),
+        ('H', 'E'),
+        ('H', 'J'),
+        ('E', 'D'),
+    ])
+    print(graph.nodes)
+    print(graph['A'])  # dict
+    print(list(graph.neighbors('A')))
+    # show_graph(graph)
+    print(bfs(graph, 'A'))
