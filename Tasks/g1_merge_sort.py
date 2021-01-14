cccffc3fd31d60de408bcca3d16f46a4e4d725b5
zl0bster <s00per@ya.ru>
2021-01-14 21:04:35 +0300

    done

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -8,4 +8,27 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+
+    def merge(contLt: List[int], contRt: List[int]) -> List[int]:
+        result = []
+        while contLt or contRt:
+            if not contLt:
+                result += contRt
+                break
+            if not contRt:
+                result += contLt
+                break
+            if contLt[0] <= contRt[0]:
+                result.append(contLt.pop(0))
+                continue
+            else:
+                result.append(contRt.pop(0))
+                continue
+        return result
+
+    if len(container) == 1:
+        return container
+    contMiddle = len(container) // 2
+    lt = container[:contMiddle]
+    rt = container[contMiddle:]
+    return merge(sort(lt), sort(rt))
